b1e22e4eacfb633d6f1fa046a24efbc94c099f8a
Jonathan Brown <jbrown@acuity.social>
2022-04-05 10:20:14 +0700

    Add Vuetify.

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,91 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import HelloWorld from './components/HelloWorld.vue'
+
+const menu = ref([
+  {
+    to: '/about',
+    icon: 'mdi-information',
+    title: 'About',
+  },
+  {
+    to: '/features',
+    icon: 'mdi-feature-search',
+    title: 'Features',
+  },
+  {
+    to: '/parachains',
+    icon: 'mdi-expansion-card-variant',
+    title: 'Parachains',
+  },
+  {
+    to: '/regenesis',
+    icon: 'mdi-rocket-launch',
+    title: 'Regenesis',
+  },
+  {
+    to: '/acu',
+    icon: 'mdi-cash-usd',
+    title: 'ACU',
+  },
+  {
+    to: '/atomic-swap',
+    icon: 'mdi-atom-variant',
+    title: 'Atomic Swap',
+  },
+  {
+    to: '/roadmap',
+    icon: 'mdi-timeline-text',
+    title: 'Roadmap',
+  },
+  {
+    to: '/tech-stack',
+    icon: 'mdi-server-network',
+    title: 'Tech Stack',
+  },
+  {
+    to: '/blog',
+    icon: 'mdi-format-quote-open',
+    title: 'Blog',
+  },
+]);
+
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <v-app id="inspire">
+    <v-navigation-drawer
+      app
+    >
+      <v-list dense>
+        <v-list-item
+          v-for="menuItem in menu"
+          :key="menuItem.icon"
+          :to="menuItem.to"
+        >
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      app
+      dark
+    >
+      <v-toolbar-title>ACUITY</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+<!--
+      <v-btn v-if="showFab" fixed dark fab bottom right color="#E6007A" to="/support">
+        <v-icon>mdi-gift</v-icon>
+      </v-btn>
+-->
+
+      <v-text-field label="Regular"></v-text-field>
+    </v-main>
+  </v-app>
 </template>
 
 <style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,34 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vuetify from '@vuetify/vite-plugin'
+
+const path = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    vuetify({
+      autoImport: true,
+    }),
+  ],
+  define: { 'process.env': {} },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  /* remove the need to specify .vue files https://vitejs.dev/config/#resolve-extensions
+  resolve: {
+    extensions: [
+      '.js',
+      '.json',
+      '.jsx',
+      '.mjs',
+      '.ts',
+      '.tsx',
+      '.vue',
+    ]
+  },
+  */
 })
