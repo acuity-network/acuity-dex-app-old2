95b055741b2678fe006e85b56449834360dce4c6
Jonathan Brown <jbrown@acuity.social>
2022-06-28 11:21:20 +0700

    Fix build.

--- src/main.ts
+++ src/main.ts
@@ -11,10 +11,14 @@ import db from './lib/db'
 import AcuityClient from './lib/AcuityClient'
 import EthClient from './lib/EthClient'
 
-let $db = db.init();
+async function start() {
+	let $db = db.init();
 
-const app = createApp(App).use(router).use(createPinia()).use(vuetify);
-app.provide('$acuityClient', await (new AcuityClient()).init());
-app.provide('$ethClient', await (new EthClient()).init($db));
-app.provide('$db', $db);
-app.mount('#app');
+	const app = createApp(App).use(router).use(createPinia()).use(vuetify);
+	app.provide('$acuityClient', await (new AcuityClient()).init());
+	app.provide('$ethClient', await (new EthClient()).init($db));
+	app.provide('$db', $db);
+	app.mount('#app');
+}
+
+start();
