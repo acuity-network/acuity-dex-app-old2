571784c8d26f8fa5796c8734565c816a43590424
Jonathan Brown <jbrown@acuity.social>
2022-11-07 20:45:53 +0700

    Remove unrequired packages.

--- vite.config.ts
+++ vite.config.ts
@@ -13,17 +13,6 @@ export default defineConfig({
     }),
   ],
   define: { 'process.env': {} },
-  resolve: {
-    alias: {
-      process: "process/browser",
-      stream: "stream-browserify",
-      zlib: "browserify-zlib",
-      util: 'util',
-      https: 'agent-base',
-      http: 'agent-base',
-//      web3: 'web3/dist/web3.min.js',  // prevent runtime TypeError in production build.
-    },
-  },
   /* remove the need to specify .vue files https://vitejs.dev/config/#resolve-extensions
   resolve: {
     extensions: [
